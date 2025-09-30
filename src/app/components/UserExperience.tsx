c38e2e9e41f09d69014a1fe4b8684fe64ab7d5a0
GCODESOfficial <godsgiftetuk2001@gmail.com>
2025-09-30 18:54:16 +0100

    user experience

--- src/app/components/UserExperience.tsx
+++ src/app/components/UserExperience.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const UserExperience: FC = () => {
   return (
-    <section className="bg-[#080C17] min-h-screen px-4 sm:px-6 md:px-20 py-12">
+    <section className="bg-[#080C17] min-h-screen px-4 sm:px-6 md:px-20 py-2 md:py-12">
       <div className="max-w-6xl mx-auto rounded-xl p-6 sm:p-8">
         {/* Label */}
         <div className="mx-auto text-center mb-12">
